454fa76087a5bccace7db9345ab0895e8bb0c0b1
firmwarecostum <145313986+firmwarecostum@users.noreply.github.com>
2026-05-18 00:11:25 +0800

    Update resource_usage_utils.h

--- include/ocudu/support/resource_usage/resource_usage_utils.h
+++ include/ocudu/support/resource_usage/resource_usage_utils.h
@@ -6,6 +6,7 @@
 #include "ocudu/adt/expected.h"
 #include "ocudu/support/resource_usage/resource_usage_metrics.h"
 #include <chrono>
+#include <cstdint>
 #include <sys/resource.h>
 
 namespace ocudu {
@@ -15,7 +16,7 @@ using rusage_meas_clock      = std::chrono::high_resolution_clock;
 using rusage_meas_time_point = rusage_meas_clock::time_point;
 using rusage_meas_duration   = std::chrono::nanoseconds;
 
-enum class rusage_measurement_type : __rusage_who_t { THREAD = RUSAGE_THREAD, PROCESS = RUSAGE_SELF, NONE };
+enum rusage_measurement_type : __rusage_who_t { THREAD = RUSAGE_THREAD, PROCESS = RUSAGE_SELF, NONE };
 
 /// Used to store the CPU time used by a thread or process, as well as process's memory usage.
 struct measurements {
